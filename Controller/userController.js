9bcd664003690385b469629af7c963679287afa7
NirajLalwani <nirajlalwani2810@gmail.com>
2024-04-01 16:55:28 +0530

    added

--- Controller/userController.js
+++ Controller/userController.js
@@ -208,7 +208,7 @@ const sendResetLink = async (req, res) => {
         <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Email Verification</title>
+          <title>Password Reset</title>
           <style>
             body {
               font-family: Arial, sans-serif;
